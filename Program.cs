b39d15207fca0d7d5e78d2911ccb3fb5c999e624
Mohamed Farook Mohamed Fazrin <mohamed.farzin@idsnext.com>
2021-08-07 19:41:59 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a word to palindrome or not?");
+            string word = Console.ReadLine();
+            string reversed = IsPalindrome(word);
+            if (word == reversed)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+        }
+
+        // Write a program in C# Sharp to check whether a string is a palindrome or not.
+
+        public static string IsPalindrome(string str)
+        {
+            string result = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == str[str.Length - 1 - i])
+                {
+                    result += str[i];
+                }
+            }
+            return result;
+        }
+    }
+
+
+
+
+
+}
---
+++ UPDATE [dbo].[SSO_UserProducts.sql
@@ -0,0 +1,5 @@
+INSERT INTO 
+[dbo].[HOLDINGCOMPANY] 
+([HOLDCOMPANY], [HOLDCOMPANYNAME], [HOLDADDRESS], 
+[HOLDCOUNTRY], [HOLDSTATE], [HOLDCITY], [HOLDZIPCODE], [UpdateType],[SyncDHBTransStatus], [SyncPMSTransStatus], 
+[SyncFCLTransStatus], [SyncMode], [IsDeleted])VALUES  ({0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12})
